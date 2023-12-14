13f7c9c0f1c15dc8a8761e71a7ef737a0491e183
Vladyslav Atanov <gibepromt@gmail.com>
2023-12-14 18:53:21 +0200

    made 404 page

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,28 @@
 'use strict';
+import { Link } from 'react-router-dom';
+import style from './NotFoundPage.module.scss';
 
 export const NotFoundPage = () => {
   return (
-    <>
-      <h1>NotFoundPage</h1>
-    </>
+    <div className={style.ErrorContainer}>
+      <div className={style.ErrorWrapper}>
+        <div className={style.iphoneX}>
+          <i>Speaker</i>
+          <b>Camera</b>
+          <s id={style.Error}>ERROR</s>
+          <s>404</s>
+          <s id={style.PageNotFound}>Page Not Found</s>
+
+          <span>Left action button</span>
+          <span>Right action button</span>
+        </div>
+
+        <div className={style.ErrorMessage}>
+          <h1 className={style.ErrorMessage__title}>Oh no!!</h1>
+          <p className={style.ErrorMessage__text}>You&apos;re either misspelling the URL or requesting a page that&apos;s no longer here.</p>
+          <Link className={style.button} to="/">Back Home</Link>
+        </div>
+      </div>
+    </div>
   );
 };
