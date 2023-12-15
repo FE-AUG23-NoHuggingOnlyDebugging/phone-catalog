f11edc82ec519266cc02ad0cc8fef5f1eecdc2cb
Tetiana Poletaieva <ptupob@gmail.com>
2023-12-15 15:41:08 +0200

    mobile catalog v2

--- src/components/Dropdown/Dropdown.tsx
+++ src/components/Dropdown/Dropdown.tsx
@@ -2,38 +2,36 @@ import React, { useState } from 'react';
 import styles from './Dropdown.module.scss';
 
 type Props = {
-  title: string;
-  current: string | null;
-  list: string[];
+  list: string[][],
+  setOn: string
+  onHandle: (setKey: string, value: string) => void,
 };
 
-const Dropdown: React.FC<Props> = ({ title, current = null, list }) => {
+const Dropdown: React.FC<Props> = ({ list, setOn, onHandle }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [current, setCurrent] = useState(list[0][1]);
 
   return (
     <div className={`${styles.catalog__dropdown} ${styles.dropdown}`}>
-      <p className={styles.dropdown__title}>{title}</p>
       <button
         className={styles.dropdown__button}
         onClick={() => setIsOpen((x) => !x)}
       >
-        <span className={styles.dropdown__current}>{current || list[0]}</span>
+        <span className={styles.dropdown__current}>{current}</span>
         <img
-          src={process.env.PUBLIC_URL + '/img/icon/arrow.svg'}
+          src={process.env.PUBLIC_URL + `${isOpen ? '/img/icons/arrow-up.svg' : '/img/icons/arrow-down.svg'}`}
           className={styles.arrow__bottom}
-          alt="Bottom Arrow"
+          alt="Arrow"
         />
       </button>
       {isOpen && (
         <ul className={styles.dropdown__list}>
-          {list.map((item: string) => (
-            <li className={styles.dropdown__item} key={item}>
-              {item}
-            </li>
+          {list.map(([key, value]) => (
+            <li className={styles.dropdown__item} key={key} onClick={() => { onHandle(setOn, key); setIsOpen(false); setCurrent(value); }}>{value}</li>
           ))}
-        </ul>
-      )}
-    </div>
+        </ul>)
+      }
+    </div >
   );
 };
 
