afd7b85fe3e0f5e67b4a46cdec781caf2809d528
Oleh <olezhkapopovych@gmail.com>
2023-12-18 15:32:05 +0200

    before merge

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './App.scss';
 import Header from './pages/Header/Header';
 import Footer from './pages/Footer/Footer';
 import { Outlet } from 'react-router-dom';
+import Breadcrumbs from './components/BreadCrumbs/BreadCrumbs';
 
 function App() {
   return (
     <>
       <Header />
+      <Breadcrumbs />
       <main>
         <Outlet />
       </main>
