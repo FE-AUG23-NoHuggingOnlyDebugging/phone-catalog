ca8d27b96e5976eda900f5dfce823982dab546fc
Semenuik Maxim <xovisage@gmail.com>
2023-12-19 22:50:17 +0200

    Catalog Card v1

--- src/components/CategoryItem/CategoryItem.tsx
+++ src/components/CategoryItem/CategoryItem.tsx
@@ -2,6 +2,7 @@ import styles from './categoryItem.module.scss';
 
 import React from 'react';
 import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
 type Props = {
   image: {
@@ -10,31 +11,34 @@ type Props = {
   };
   title: string;
   model: string;
-  modelCount: number;
+  totalModel: number;
 };
 
-const CategoryItem: React.FC<Props> = ({ image, title, model, modelCount }) => (
-  <li className={cn(styles.category_list__item, styles.category_item)}>
-    <div
-      className={cn(
-        styles.category_item__bg,
-        styles[`category_item__bg_${model}`],
-      )}
-    >
-      <img
-        src={process.env.PUBLIC_URL + image.src}
-        alt={image.alt}
+const CategoryItem: React.FC<Props> = ({ image, title, model, totalModel }) => {
+  return (
+    <li className={cn(styles.category_list__item, styles.category_item)}>
+      <Link to={'catalog/' + model} className={styles.link__cover} />
+      <div
         className={cn(
-          styles.category_item__image,
-          styles[`category_item__image_${model}`],
+          styles.category_item__bg,
+          styles[`category_item__bg_${model}`],
         )}
-      />
-    </div>
+      >
+        <img
+          src={image.src}
+          alt={image.alt}
+          className={cn(
+            styles.category_item__image,
+            styles[`category_item__image_${model}`],
+          )}
+        />
+      </div>
 
-    <h3 className={styles.category_item__title}>{title}</h3>
+      <h3 className={styles.category_item__title}>{title}</h3>
 
-    <p className={styles.category_item__model}>{`${modelCount} models`}</p>
-  </li>
-);
+      <p className={styles.category_item__model}>{`${totalModel} models`}</p>
+    </li>
+  );
+};
 
 export default CategoryItem;
---
+++ src/types/Categories.ts
@@ -0,0 +1,6 @@
+export type Categories = {
+  id: string,
+  url: string,
+  alt: string,
+  total: number,
+};
