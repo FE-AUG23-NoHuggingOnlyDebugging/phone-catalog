593f9ec19fff4186a6bbc7d3bf6e43aebeadda1f
Oleh <olezhkapopovych@gmail.com>
2023-12-20 23:23:06 +0200

    after add color theme

--- src/components/CartIcon/CartIcon.tsx
+++ src/components/CartIcon/CartIcon.tsx
@@ -10,22 +10,25 @@ const CartIcon: React.FC = () => {
 
   return (
     <div className={styles.cart_icon}>
-<!-- <<<<<<< light-dark-theme
+      {/* <!-- <<<<<<< light-dark-theme
       <img
         className={styles.dark_theme_icon_color}
         src={process.env.PUBLIC_URL + '/img/icons/cart.svg'}
         alt="Cart"
       />
 <!--       {count > 0 && <div className={styles.counter}>{count}</div>} -->
-======= -->
-      <img src={process.env.PUBLIC_URL + '/img/icons/cart.png'} alt="Cart" />
-      {count > 0 &&
+======= --> */}
+      <img
+        src={process.env.PUBLIC_URL + '/img/icons/cart.svg'}
+        className={styles.dark_theme_icon_color}
+        alt="Cart"
+      />
+      {count > 0 && (
         <div className={styles.circle}>
-          <span className={styles.counter}>
-            {count}
-          </span>
-        </div>}
-<!-- >>>>>>> main -->
+          <span className={styles.counter}>{count}</span>
+        </div>
+      )}
+      {/* <!-- >>>>>>> main --> */}
     </div>
   );
 };
